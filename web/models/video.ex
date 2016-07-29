53daa888e2e999fa60ca64b00233fcae1dd76b69
Jon Miller <jon4988@gmail.com>
2016-07-29 17:52:52 -0400

    Create slug, player for videos, finish Ch 9

--- web/models/video.ex
+++ web/models/video.ex
@@ -7,6 +7,7 @@ defmodule Rumbl.Video do
     field :description, :string
     belongs_to :user, Rumbl.User
     belongs_to :category, Rumbl.Category
+    field :slug, :string
 
     timestamps()
   end
@@ -14,10 +15,31 @@ defmodule Rumbl.Video do
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
-  def changeset(struct, params \\ %{}) do
-    struct
+  def changeset(model, params \\ %{}) do
+    model
     |> cast(params, [:url, :title, :description, :category_id])
+    |> slugify_title()
     |> assoc_constraint(:category)
     |> validate_required([:url, :title, :description])
+  end
+
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else
+      changeset
+    end
+  end
+
+  defp slugify(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/u, "-")
+  end
+end
+
+defimpl Phoenix.Param, for: Rumbl.Video do
+  def to_param(%{slug: slug, id: id}) do
+    "#{id}-#{slug}"
   end
 end
